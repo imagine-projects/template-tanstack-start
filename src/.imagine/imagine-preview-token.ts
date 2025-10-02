2aa64a39e37eea1f07b407c4dc36eb3c8aa3565f
Ariel Weinberger <weinberger.ariel@gmail.com>
2025-10-02 01:12:54 -0500

    add imagine preview token

---
+++ src/.imagine/imagine-preview-token.ts
@@ -0,0 +1,49 @@
+/**
+ * @imagine-hidden
+ * @imagine-readonly
+ */
+import { setSessionCookies } from '@/server/functions/auth'
+import { Account, Client, Models, Users } from 'node-appwrite'
+
+export async function handleEmbeddedPreview(
+  imaginePreviewToken: string,
+): Promise<Models.User | null> {
+  // If it is, we generate a session token for the user.
+  let userId: string
+  // First, we check if the token is valid and obtain the user ID
+  const tokenUser =
+    await getAppwriteUserFromImaginePreviewToken(imaginePreviewToken)
+  userId = tokenUser.$id
+  // Then, we generate a session for the user
+  const session = await createUserSession(userId)
+  // Then, we attach the session to the cookie
+  setSessionCookies({ data: { id: session.$id, secret: session.secret } })
+  return tokenUser
+}
+
+async function createUserSession(userId: string) {
+  const adminClient = getProjectAdminClient()
+  const users = new Users(adminClient)
+  const session = await users.createSession({ userId })
+  return session
+}
+
+function getProjectAdminClient() {
+  const client = new Client()
+    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT!)
+    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID!)
+    .setKey(import.meta.env.VITE_APPWRITE_API_KEY!)
+  return client
+}
+
+async function getAppwriteUserFromImaginePreviewToken(
+  imaginePreviewToken: string,
+) {
+  const client = new Client()
+    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT!)
+    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID!)
+    .setJWT(imaginePreviewToken)
+  const account = new Account(client)
+  const tokenUser = await account.get()
+  return tokenUser
+}
