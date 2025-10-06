4a357f3afcbe7bdafdfaca0eade8f2d387a99b6e
Suhas Deshpande <suhas2u@gmail.com>
2025-10-06 17:16:53 -0500

    refactor public route by removing unused state and server functions

--- src/routes/_public/index.tsx
+++ src/routes/_public/index.tsx
@@ -1,38 +1,14 @@
 import { ImaginePlaceholder } from '@/components/imagine-placeholder'
-import { createFileRoute, useLoaderData } from '@tanstack/react-router'
-import { Button } from '@/components/ui/button'
-import { useServerFn } from '@tanstack/react-start'
-import { doSomethingFn, preloadDataFn } from '@/server/functions/do-something'
-import { useState } from 'react'
+import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_public/')({
   component: Index,
-  loader: async () => {
-    const preloadedData = await preloadDataFn()
-    return {
-      preloadedData,
-    }
-  },
 })
 
 function Index() {
-  const [str, setStr] = useState('NOT YET DEFINED')
-  const doSomething = useServerFn(doSomethingFn)
-  const { preloadedData } = useLoaderData({ from: Route.id })
-
-  const handleSomething = async () => {
-    const result = await doSomething({ data: { name: 'John Doe' } })
-    setStr(result)
-  }
-
   return (
     <div className="min-h-screen flex flex-col p-8">
       <ImaginePlaceholder />
-      <div>
-        <Button onClick={handleSomething}>Do something</Button>
-        <div>String: {str}</div>
-        <div>Preloaded data: {preloadedData}</div>
-      </div>
     </div>
   )
 }
