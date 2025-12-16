63b501db8892ee5ed7cd29de0c0f193212e1c960
Ariel Weinberger <Weinberger.Ariel@gmail.com>
2025-12-15 21:15:58 -0600

    feat: lint checks with auto fix (#12)
    * feat: lint checks with auto fix
    
    * more fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,27 @@
-//  @ts-check
+import unusedImports from 'eslint-plugin-unused-imports'
+import tseslint from 'typescript-eslint'
 
-// import { tanstackConfig } from '@tanstack/eslint-config'
-
-export default [
-  //...tanstackConfig
-]
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+export default tseslint.config(
+  {
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+      'src/components/ui/**',
+      '.output',
+      '.nitro',
+    ],
+  },
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+      'unused-imports/no-unused-imports': 'error',
+    },
+  },
+)
