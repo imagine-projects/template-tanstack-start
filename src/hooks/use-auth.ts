60831c52761da4c3b3884116fb9360fb12c9f266
Ariel Weinberger <weinberger.ariel@gmail.com>
2025-12-25 20:08:34 -0600

    fix: formatting

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -6,7 +6,7 @@ import { Models } from 'node-appwrite'
 export function useAuth() {
   const { currentUser } = useLoaderData({ from: '__root__' }) as {
     currentUser: Models.User<Models.Preferences>
-  };
+  }
   const signOut = useServerFn(signOutFn)
 
   return {
