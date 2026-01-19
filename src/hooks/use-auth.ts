7739fde0f1f57d4530be3ad9353488d9052349a0
eldadfux <eldad.fux@gmail.com>
2026-01-19 19:36:50 +0100

    feat: enhance authentication flow with password recovery and reset functionality
    - Updated README.md to include instructions for running the app in development mode and added details about the authentication features.
    - Added routes for password recovery and reset in routeTree.gen.ts.
    - Modified use-auth.ts to handle sign-out and session invalidation.
    - Updated ImaginePlaceholder component to streamline sign-out functionality.
    - Enhanced sign-in page with a link to the forgot password page.
    - Refactored sign-out function to use GET method and improved error handling.
    - Implemented forgotPasswordFn and resetPasswordFn in auth.ts for handling password recovery and reset processes.

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -1,5 +1,5 @@
 import { signOutFn } from '@/server/functions/auth'
-import { useLoaderData } from '@tanstack/react-router'
+import { useLoaderData, useRouter } from '@tanstack/react-router'
 import { useServerFn } from '@tanstack/react-start'
 import { Models } from 'node-appwrite'
 
@@ -7,7 +7,13 @@ export function useAuth() {
   const { currentUser } = useLoaderData({ from: '__root__' }) as {
     currentUser: Models.User<Models.Preferences>
   }
-  const signOut = useServerFn(signOutFn)
+  const signOutServer = useServerFn(signOutFn)
+  const router = useRouter()
+
+  const signOut = async () => {
+    await signOutServer()
+    await router.invalidate()
+  }
 
   return {
     currentUser,
